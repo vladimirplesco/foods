afe66f6df435e4b086d91436e954e93c9a68663e
Vladimir Plesco <pleshco@deeplace.md>
2024-02-16 08:30:32 +0200

    next step

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,8 +1,8 @@
 export const sortDishes = (dishes) => {
-  // dishes.sort((a, b) => {
-  //   if (a.data.name > b.data.name) return 1;
-  //   else if (a.data.name < b.data.name) return -1;
-  //   else return 0;
-  // });
+  dishes.sort((a, b) => {
+    if (a.data.name > b.data.name) return 1;
+    else if (a.data.name < b.data.name) return -1;
+    else return 0;
+  });
   return dishes;
 };
