71ed86ee67269172a7b7a908782caf846b486787
Vladimir Plesco <pleshco@deeplace.md>
2024-02-21 11:15:54 +0200

    wednesday once more

--- src/assets/js/foods.js
+++ src/assets/js/foods.js
@@ -5,24 +5,13 @@ import persist from "@alpinejs/persist";
 Alpine.plugin(persist);
   
   Alpine.data("dishes", function () {
+   
     return {
       view: this.$persist("dishes"),
       search: this.$persist(""),
-
-      // get dishes() {
-      //   const term = this.search.toLowerCase();
-      //   return this.dishes
-      //     .filter((dish) => !this.select.includes(dish.slug))
-      //     .filter((dish) => dish.data.name.toLowerCase().includes(term));
-      // },
-      // clearImmediatelearAll() {
-      //   this.hide = false;
-      //   // this.buy = [];
-      //   // this.custom = [];
-      //   // this.select = [];
-      //   // this.search = "";
-      // },
-  
+      get dishes() {
+        return dishes;
+      },
     };
   });
   Alpine.start();
